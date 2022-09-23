423d14537343aea9a98a0a03b67e5db90e7f9557
genos322 <genos322@gmail.com>
2022-09-23 13:04:51 -0500

    first com

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const app = express()
+
+const http = require('http')
+const server = http.createServer(app)
+
+const {Server} = require('socket.io')
+const io = new Server(server)
+
+io.on('connection', ( socket)=>{
+    // console.log('Un usuario conectado')
+    // socket.on('disconnect', ()=>{
+    //     console.log('usuario desconectado')
+    // })
+    socket.on('chat' , (msg) => {
+        io.emit('chat', msg)
+         console.log('Mensaje: '+msg)
+    })
+})
+
+app.get('/',(req,res) =>{
+    // res.send('<h1>App Cha</h1>')
+    console.log(__dirname)
+    res.sendFile(`${__dirname}/cliente/index.html`)
+
+})
+
+server.listen(3000,()=>{
+    console.log('Servidor corriendo en http://localhost:3000')
+})
